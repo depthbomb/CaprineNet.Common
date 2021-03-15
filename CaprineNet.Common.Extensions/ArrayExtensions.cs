cf7f8f00952d959503060e387e4ec2ca489a6fef
depthbomb <reesepup77@hotmail.com>
2021-03-15 18:04:22 -0500

    Add IndexOf array extension

---
+++ CaprineNet.Common.Extensions/ArrayExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CaprineNet.Common.Extensions
+{
+    public static class ArrayExtensions
+    {
+        /// <summary>
+        /// Returns the index of an array item
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="array"></param>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static int IndexOf<T>(this T[] array, T value) => Array.IndexOf(array, value);
+    }
+}
